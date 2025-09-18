e77e4ecfba7b20dbc5f673e42ca90222f20160ba
TAS3-maker <dm@techarchsoftwares.com>
2025-09-18 14:14:29 +0530

    Update auth.js

--- src/main/api/auth.js
+++ src/main/api/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+const API_URL = process.env.REACT_APP_API_URL || "https://hintswork-backend.vercel.app/api";
 
 export const loginApi = async (email, password) => {
   const { data } = await axios.post(`${API_URL}/auth/login`, { email, password });
